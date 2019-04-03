553ba42ac8427c17ebbe1ec065f7e06ce2a30478
nawaar.zakee <nawaar.zakee@yahoo.com>
2019-04-03 10:38:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
+document.addEventListener('DOMContentLoaded', function(event) {
+  Handlebars.registerPartial(
+    'authorPartial',
+    document.getElementById('author-partial-template').innerHTML
+  );
+});
 
 function getRepositories() {
-  var request = new XMLHttpRequest();
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', showRepositories);
+  req.open('GET', 'https://api.github.com/users/octocat/repos');
+  req.send();
+}
 
-request.open('GET', 'https://tv-v2.api-fetch.website/random/movie');
-
-request.onreadystatechange = function () {
-  if (this.readyState === 4) {
-    console.log('Status:', this.status);
-    console.log('Headers:', this.getAllResponseHeaders());
-    console.log('Body:', this.responseText);
-  }
-};
-
-request.send();
+function showRepositories(event, data) {
+  const repos = JSON.parse(this.responseText);
+  const src = document.getElementById('repository-template').innerHTML;
+  const template = Handlebars.compile(src);
+  const repoList = template(repos);
+  document.getElementById('repositories').innerHTML = repoList;
 }
