9b4ec78762c064e3b4b445315b5b45f55f66e743
nawaar.zakee <nawaar.zakee@yahoo.com>
2019-04-03 10:35:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-document.addEventListener('DOMContentLoaded', function(event) {
-  Handlebars.registerPartial(
-    'authorPartial',
-    document.getElementById('author-partial-template').innerHTML
-  );
-});
 
 function getRepositories() {
   var request = new XMLHttpRequest();
@@ -21,10 +15,3 @@ request.onreadystatechange = function () {
 request.send();
 }
 
-function showRepositories(event, data) {
-  const repos = JSON.parse(this.responseText);
-  const src = document.getElementById('repository-template').innerHTML;
-  const template = Handlebars.compile(src);
-  const repoList = template(repos);
-  document.getElementById('repositories').innerHTML = repoList;
-}
